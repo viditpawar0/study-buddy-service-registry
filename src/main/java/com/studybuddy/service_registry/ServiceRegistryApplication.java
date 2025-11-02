f33ded361abb319708b0ddeefbc4dcd636c30302
Vidit Pawar <viditpawar0@gmail.com>
2025-11-02 21:16:05 +0530

    moved server port to default profile

--- src/main/java/com/studybuddy/service_registry/ServiceRegistryApplication.java
+++ src/main/java/com/studybuddy/service_registry/ServiceRegistryApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class ServiceRegistryApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
     }
 
